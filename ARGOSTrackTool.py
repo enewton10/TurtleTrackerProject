f5b0c7d1487c0537e53a2ffee35f89df9fa1d9b4
enewton10 <eric.newton@duke.edu>
2024-10-19 18:05:50 -0400

    allow user to set date and show location

--- ARGOSTrackTool.py
+++ ARGOSTrackTool.py
@@ -9,6 +9,9 @@
 # Date:   Fall 2024
 #--------------------------------------------------------------
 
+#Ask user for a date
+user_date = input("Enter a date: ")
+
 #Create a variable pointing to the data file
 file_name = './data/raw/Sara.txt'
 
@@ -34,7 +37,6 @@ for lineString in line_list[17:]:
     #Split the string into a list of data items
     lineData = lineString.split()
 
-
     #Extract items in list into variables
     record_id = lineData[0]
     obs_date = lineData[2]
@@ -48,5 +50,17 @@ for lineString in line_list[17:]:
         date_dict[record_id] = obs_date
         location_dict[record_id] = (obs_lat, obs_lon)
 
-    #Print the location of sara
-    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+#Initialize key list
+keys = []
+
+#Loop through items in date_dict
+for key, value in date_dict.items():
+    if value == user_date:
+        keys.append(key)
+
+#Loop through keys and report locations
+for key in keys:
+    location = location_dict[key]
+    lat = location[0]
+    long = location[1]
+    print(f"On {user_date}, Sara the turtle was seen at {lat}d Lat, {long}d Lng")
