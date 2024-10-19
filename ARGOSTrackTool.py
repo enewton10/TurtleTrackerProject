53341b3b982ce3e89b289fb19b6e5c5cbfe94997
enewton10 <eric.newton@duke.edu>
2024-10-19 17:13:19 -0400

    Revert "switch for loop for while loop"
    This reverts commit fda2112d5155ff1593ae4f5364da518568ab677b.

--- ARGOSTrackTool.py
+++ ARGOSTrackTool.py
@@ -12,20 +12,20 @@
 #Create a variable pointing to the data file
 file_name = './data/raw/Sara.txt'
 
-
 #Create a file object from the file
 file_object = open(file_name,'r')
 
 #Read contents of file into a list
-lineString = file_object.readline()
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
 
 #Pretend we read one line of data from the file
-while lineString:
-        
+for lineString in line_list[17:]:
     if lineString[0] in ('#', "u"):
-        lineString = file_object.readline()
         continue
- 
+
     #Split the string into a list of data items
     lineData = lineString.split()
 
@@ -39,5 +39,3 @@ while lineString:
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-    lineString = file_object.readline()
