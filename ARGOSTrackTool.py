2ad016a713e0b7d63860e0c6bfcb59c6dc0a1650
enewton10 <eric.newton@duke.edu>
2024-10-19 17:26:41 -0400

    create dictionaries and add items

--- ARGOSTrackTool.py
+++ ARGOSTrackTool.py
@@ -21,8 +21,13 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+#Initialize dictionaries
+date_dict = {}
+location_dict = {}
+
 #Pretend we read one line of data from the file
 for lineString in line_list[17:]:
+    #Check if line is a data line
     if lineString[0] in ('#', "u"):
         continue
 
@@ -37,5 +42,9 @@ for lineString in line_list[17:]:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
+    #Add items to dictionaries
+    date_dict[record_id] = obs_date
+    location_dict[record_id] = (obs_lat, obs_lon)
+
     #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
